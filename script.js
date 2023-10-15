f0cd13acca254a0357901f289bcfc8b2a204b7bb
Param-Veer-Singh <122340319+Param-Veer-Singh@users.noreply.github.com>
2023-10-15 09:58:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function decimalToBinary(num) {
   //Write you code here
-  let ans = "";
+  let ans = '';
    while(num > 0){
       let rem = num % 2;
       ans = rem + ans;
 
       num = Math.floor(num/2);
    }
-   return ans;	
+   return ans;
 }
 
 window.decimalToBinary = decimalToBinary;
